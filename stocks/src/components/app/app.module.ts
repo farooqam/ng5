c486a43eb7e50865cfd607076450b9badfcaad70
Farooq Mahmud <farooq@techniqly.com>
2018-03-24 12:20:53 -0700

    Routing

--- stocks/src/components/app/app.module.ts
+++ stocks/src/components/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormBuilder, ReactiveFormsModule  } from '@angular/forms';
 import {NgbModule, NgbTooltip} from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutes } from './app.routes';
 import { AppComponent } from './app.component';
 import { SummaryComponent } from '../summary/summary/summary.component';
 import { StockService } from '../../services/stock/stock.service';
@@ -23,7 +24,8 @@ import { ManageComponent } from '../user/profile/manage/manage.component';
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
-    NgbModule.forRoot() 
+    NgbModule.forRoot(),
+    AppRoutes
   ],
   providers: [FormBuilder, NgbTooltip, StockService, StockServiceSettings],
   bootstrap: [AppComponent]
---
+++ stocks/src/components/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { ManageComponent } from '../user/profile/manage/manage.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: DashboardComponent
+    },
+    {
+        path: 'manage',
+        component: ManageComponent
+    }
+];
+
+export const AppRoutes = RouterModule.forRoot(routes);
