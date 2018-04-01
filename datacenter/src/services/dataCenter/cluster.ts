5c98d6ee7119a322830a41c57dbcb290ad3b936b
farooqam <farooq_am@hotmail.com>
2018-04-01 15:32:44 -0700

    WIP - dashboard component

--- datacenter/src/services/dataCenter/cluster.ts
+++ datacenter/src/services/dataCenter/cluster.ts
@@ -1,8 +1,14 @@
 import { ClusterNode } from "./clusterNode";
 import { Threshold } from "./threshold";
+import { ClusterStatus } from "./clusterStatus.enum";
 
 export interface Cluster {
     name: string;
     clusterNodes: Array<ClusterNode>;
     thresholds: Array<Threshold>;
+    status: ClusterStatus
+
+    nodeCount: number {
+        return this.clusterNodes.length;
+    }
 }
